93dcc73eeca6e38c2b48b91212a7e9b35de6203d
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2022-04-07 10:27:36 +1000

    init

---
+++ src/subfolder/src/test2.ts
@@ -0,0 +1,6 @@
+import { customElement, noView } from 'aurelia-framework';
+
+@customElement('test2-template')
+export class Test2 {
+
+}
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+import { customElement, noView } from 'aurelia-framework';
+
+@customElement('test-element-template')
+@noView()
+export class Test {
+
+}
